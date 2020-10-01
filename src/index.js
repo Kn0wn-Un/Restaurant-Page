9d9c4c9e164f96da6f3d26885fcf3fd2037ac29c
Kn0wn-Un <naikdarsh175@gmail.com>
2020-10-01 16:30:36 +0530

    Script

--- src/index.js
+++ src/index.js
@@ -1,3 +1,53 @@
-//let temp = document.querySelector("div");
-//temp.innerHTML = "Using NPM and WEBPACK";
-console.log("initial commit");
+
+const tLinks = ['Home', 'Menu', 'About'];
+const container = document.querySelector(".container");
+const addHeading = ()=>{
+    let rName = document.createElement("h1");
+    rName.innerHTML = "Restaurant Name";
+    container.appendChild(rName);
+}
+
+const addTabs = ()=>{
+    let tContainer = document.createElement("div");
+    tContainer.classList.add("tabs-container");
+    let tHeading = document.createElement("div");
+    tHeading.classList.add("tabs-headings");
+    populateHeadings();
+    let tContents = document.createElement("div");
+    tContents.classList.add("tabs-contents");
+
+    tContainer.appendChild(tHeading);
+    tContainer.appendChild(tContents);
+    container.appendChild(tContainer);
+}
+
+const populateHeadings = ()=>{
+    let tHeading = document.querySelector(".tabs-headings");
+    for(let i = 0; i < tLinks.length; i++){
+        let h = document.createElement("div");
+        h.innerHTML = tLinks[i];
+        h.id = tLinks[i];
+        //h.addEventListener("click", (e)=>{displayContent(e)});
+        tHeading.appendChild(h);
+    }
+}
+
+const displayContent = (e)=>{
+    console.log(e);
+}
+const init = ()=>{
+    addHeading();
+    addTabs();
+}
+init();
+//return {init, addHeading, addTabs}
+
+
+
+/*const addHeading = ()=>{
+    let rName = document.createElement("h1");
+    rName.innerHTML = "Restaurant Name";
+    const container = document.querySelector(".container");
+    container.appendChild(rName);
+}
+addHeading();*/
